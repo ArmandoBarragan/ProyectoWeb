94755876400321e5ca8be6d571bb67c5c9b15dc8
Armando Barragán <armando.barragan2@outlook.com>
2020-11-14 11:05:41 -0700

    Commit antes de collectstatic

--- proyectosDjango/settings.py
+++ proyectosDjango/settings.py
@@ -122,12 +122,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# STATIC_ROOT = 'static'
-
 STATICFILES_DIRS = (
   os.path.join(BASE_DIR, 'static'),
 )
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, '')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
