5f7f7c7ff0e8e096b0dfd277adaeb331282acd37
Armando Barragán <armando.barragan2@outlook.com>
2020-11-17 10:07:33 -0700

    Ajax_project finished, started login app

--- ajax_project/urls.py
+++ ajax_project/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.ajax_search, name='search'),
+    path('', views.index, name='index'),
+    path('search/', views.search, name='search')
 ]
--- ajax_project/views.py
+++ ajax_project/views.py
@@ -1,16 +1,43 @@
 from django.shortcuts import render
 from django import forms
 from .models import Person
+from django.http import JsonResponse, HttpResponse
+import json
+
 # Create your views here.
 class PersonForm(forms.ModelForm):
     class Meta:
         model = Person
         fields = ['name', 'last_name', 'job', 'salary']
 
-def ajax_search(request):
+def index(request):
+    if request.method == "POST":
+        form = PersonForm(data=request.POST)
+        form.save()
+    
     context = {
         'form': PersonForm()
     }
-    
     return render(request, 'ajax/search.html', context=context)
 
+def search(request):
+    if request.method == "GET":
+        try:
+            name = request.GET.get('name')
+            query = Person.objects.filter(name__startswith=name)
+            people = [people_serializer(person) for person in query]
+            print(people)
+
+            return HttpResponse(json.dumps(people), content_type='application/json')
+
+        except ValueError as error:
+            return HttpResponse(error)
+
+def people_serializer(person):
+    data = {
+        'name':person.name,
+        'last_name': person.last_name,
+        'job': person.job,
+        'salary': person.salary,
+    }
+    return data
